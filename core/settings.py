c373b698a5676f7b5fece3b0e4bdabf5afdf1661
RJaqE <jaquecl@gmail.com>
2025-12-17 02:41:49 -0300

    Configuración de producción para Render (Whitenoise + PostgreSQL)

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+#STATIC_URL = 'static/'
 STATIC_URL = 'static/'
+
+# Esto le dice a Whitenoise donde juntar los archivos cuando ejecutamos collectstatic
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Redirecciones de Login/Logout
