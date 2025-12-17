6baf5524b722a74ec42e8c1a334f2806390adcd9
RJaqE <jaquecl@gmail.com>
2025-12-17 03:22:12 -0300

    Automatizar creación de superusuario

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ python manage.py collectstatic --no-input
 
 # Aplicar migraciones a la base de datos de la nube
 python manage.py migrate
+
+# --- LÍNEA NUEVA ---
+# Crea el superusuario automáticamente si no existe (usando las variables que pusimos en Render)
+python manage.py createsuperuser --noinput || echo "El superusuario ya existe, saltando creación."
